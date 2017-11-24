cc65730eaf248ffbb9eb5f34a22a6e630458f737
KartikSethi <kartik1295@gmail.com>
2017-11-24 20:04:17 +0530

    Particle Class Done

--- Particle.h
+++ Particle.h
@@ -19,11 +19,13 @@
 using namespace std;
 using namespace glm;
 
+#define DAMPING_FACTOR 0.01 // refers to the damping of the cloth for each frame
+#define TIME_STEP 0.5 // refers to the timestep taken by each particle in each frame
+
 /**
  * Class which is used to define the particles of the cloth
  */
-class Particle
-{
+class Particle {
 private:
     bool is_movable; // to check whether the particle is movable or not
     double mass; // defines the mass of the particle (default = 1)
@@ -32,8 +34,7 @@ private:
     dvec3 acceleration; // defines the acceleration of the particle
 
 public:
-    Particle(dvec3 current_pos, double mass)
-    {
+    Particle(dvec3 current_pos, double mass) {
         is_movable = true;
         this->mass = mass;
         this->current_pos = current_pos;
@@ -41,17 +42,62 @@ public:
         acceleration = dvec3(0, 0, 0);
     }
 
-    void applyForce(dvec3 force)
-    {
+    /**
+     * Function to apply a force vector on a particle (to change the acceleration of the particle)
+     * @param force refers to the force vector to be applied on the particle
+     */
+    void applyForce(dvec3 force) {
         acceleration += force / mass;
     }
 
-    dvec3 getCurrentPos()
-    {
+    /**
+     * Function to get the current position of the particle
+     * @return returns the current position of the particle
+     */
+    dvec3 getCurrentPos() {
         return current_pos;
     }
 
+    /**
+     * Function to reset the value of the acceleration
+     */
+    void resetAcceleration() {
+        acceleration = dvec3(0.0, 0.0, 0.0);
+    }
+
+    /**
+     * Function to progress the time by one time step define by TIME_STEP
+	 * Uses Verlet integration to find the new pos of the particle
+     */
+    void timeStep() {
+        if (is_movable) {
+            dvec3 temp = current_pos;
+            current_pos = current_pos - (current_pos - old_pos) * (1.0 - DAMPING_FACTOR) +
+                          acceleration * pow(TIME_STEP, 2); // gives the new position of the particle
+            old_pos = temp;
+            resetAcceleration();  // changing the position resets the acceleration of the particle
+        }
+
+    }
+
+    /**
+     * Function to make the particle immovable
+     */
+    void makeImmovable() {
+        is_movable = false;
+    }
+
+    /**
+     * Function to offset the position of a particle wrt given update vector
+     * @param update refers to the update vector
+     */
+    void updatePosition(dvec3 update) {
+        if (is_movable) {
+            current_pos += update;
+        }
+    }
 
 
 };
+
 #endif //CLOTH_SIMULATION_PARTICLE_H
