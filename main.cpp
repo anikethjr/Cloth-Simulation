2ea6b79030bea7a2d4c93d8cdfb22b22b2b38069
Aniketh Reddy <anikethjr@gmail.com>
2017-11-24 18:27:06 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by anikethjr on 24/11/17.
+//
+
+#include <bits/stdc++.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+
+#define PI 3.14159265
+
+using namespace std;
+using namespace glm;
+
+int main()
+{
+    cout<<"Hello world";
+    return 0;
+}
+
