36fe2bd2d5fe3cdb7418864ea56ee452ecf02973
Aniketh Reddy <anikethjr@gmail.com>
2017-11-24 18:40:29 +0530

    Initial Commit

---
+++ Particle.h
@@ -0,0 +1,17 @@
+//
+// Created by anikethjr on 24/11/17.
+//
+
+#ifndef CLOTH_SIMULATION_PARTICLE_H
+#define CLOTH_SIMULATION_PARTICLE_H
+
+/**
+ * Class which is used to define the particles of the cloth
+ */
+class Particle
+{
+
+public:
+
+};
+#endif //CLOTH_SIMULATION_PARTICLE_H
